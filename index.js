0e69730bde15e676e6f4967e100dc3b712406f30
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-26 22:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
   return "I can't hear you!"
- 
-} if (string.toUpperCase() == string)
+} 
+if (string.toUpperCase() == string)
   return "Yes Indeed!"
-  
-} if (string === "I love you, Grandma")
+ } 
+if (string === "I love you, Grandma")
   return "I love you, too"
   }
 
