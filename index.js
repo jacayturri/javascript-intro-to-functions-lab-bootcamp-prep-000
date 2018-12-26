ef81215bc8c34f342151bb2c51a026af955b7992
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-26 22:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function sayHiToGrandma(string) {
 } if (string.toUpperCase() == string)
   return "Yes Indeed!"
   
-} if (string)
+} if (string === "I love you, Grandma")
+  return "I love you, too"
+  }
+  
+
   
   
   
